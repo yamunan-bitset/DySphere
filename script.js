f8d8ee8b666d41e524cf2fa6129226321d640093
Anirudh <yamunan.bitset@gmail.com>
2025-02-02 18:21:48 +0100

    switch user button; changes text-align

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const window = document.getElementById('transparent-window');
+    window.style.display = 'block';
+});
+
 function toggleWindow() {
     const window = document.getElementById('transparent-window');
     if (window.style.display === 'none' || window.style.display === '') {
@@ -5,6 +10,44 @@ function toggleWindow() {
     } else {
         window.style.display = 'none';
     }
+}
+
+function getStyle(el, styleProp) {
+    var value, defaultView = (el.ownerDocument || document).defaultView;
+    // W3C standard way:
+    if (defaultView && defaultView.getComputedStyle) {
+      // sanitize property name to css notation
+      // (hypen separated words eg. font-Size)
+      styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
+      return defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
+    } else if (el.currentStyle) { // IE
+      // sanitize property name to camelCase
+      styleProp = styleProp.replace(/\-(\w)/g, function(str, letter) {
+        return letter.toUpperCase();
+      });
+      value = el.currentStyle[styleProp];
+      // convert other units to pixels on IE
+      if (/^\d+(em|pt|%|ex)?$/i.test(value)) { 
+        return (function(value) {
+          var oldLeft = el.style.left, oldRsLeft = el.runtimeStyle.left;
+          el.runtimeStyle.left = el.currentStyle.left;
+          el.style.left = value || 0;
+          value = el.style.pixelLeft + "px";
+          el.style.left = oldLeft;
+          el.runtimeStyle.left = oldRsLeft;
+          return value;
+        })(value);
+      }
+      return value;
+    }
+  }
+
+var current_align = 'left';
+
+function switch_user() {
+    const displayAreas = document.querySelectorAll('.display-area');
+    var align_current = getStyle(displayAreas[displayAreas.length - 1], 'text-align');
+    current_align = align_current === 'left' ? 'right' : 'left';
 }
 
 var n = 0;
@@ -20,5 +63,13 @@ function send() {
     displayArea.style.position = 'absolute';
     displayArea.style.top = `${n * 50}px`;
     displayArea.style.left = '10px';
+    displayArea.style.textAlign = current_align;
     transparentWindow.insertBefore(displayArea, transparentWindow.firstChild);
 }
+
+document.getElementById('enter-msg-box').addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        send();
+        this.value = '';
+    }
+});
